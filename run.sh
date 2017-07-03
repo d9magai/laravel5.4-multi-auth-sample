c90f9e500c05e5126126fc0379d2bc050d212e83
d9magai <kumagai@sencorp.co.jp>
2017-07-03 06:36:42 +0000

    add --replacepkgs option

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ cd `dirname $0`
 
 if [ -n "$(command -v yum)" ]; then
     sudo yum install -y epel-release
-    sudo rpm -ivh https://rpms.remirepo.net/enterprise/remi-release-7.rpm
+    sudo rpm -ivh --replacepkgs https://rpms.remirepo.net/enterprise/remi-release-7.rpm
     sudo yum install -y --enablerepo=remi-php71 vim git nkf colordiff docker httpd zsh php php-cli php-mbstring php-xml php-pecl-zip php-pdo php-mysql php-pecl-xdebug
     sudo systemctl enable docker
     sudo systemctl start docker
