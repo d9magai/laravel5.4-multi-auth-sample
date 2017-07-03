845cd68a66db16a9652dcacfa4ad979b77737bf1
d9magai <kumagai@sencorp.co.jp>
2017-07-03 09:00:08 +0000

    create a route to handle the POST data and do validation

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,5 +35,24 @@ Route::get('/', function () {
 
 Route::get('/submit', function () {
     return view('submit');
+});
+
+Route::post('/submit', function(Request $request) {
+    $validator = Validator::make($request->all(), [
+        'title' => 'required|max:255',
+        'url' => 'required|max:255',
+        'description' => 'required|max:255',
+    ]);
+    if ($validator->fails()) {
+        return back()
+            ->withInput()
+            ->withErrors($validator);
+    }
+    $link = new \App\Link;
+    $link->title = $request->title;
+    $link->url = $request->url;
+    $link->description = $request->description;
+    $link->save();
+    return redirect('/');
 });
 
