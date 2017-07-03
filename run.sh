e170559cb23f386b70ad7ff59122518affe301ae
d9magai <kumagai@sencorp.co.jp>
2017-07-03 04:12:28 +0000

    add run.sh

---
+++ run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -eux
+cd `dirname $0`
+
+if [ -n "$(command -v yum)" ]; then
+    sudo yum install -y epel-release
+    sudo rpm -ivh https://rpms.remirepo.net/enterprise/remi-release-7.rpm
+    sudo yum install -y --enablerepo=remi-php71 vim git nkf colordiff docker httpd zsh php php-cli php-mbstring php-xml php-pecl-zip php-pdo php-mysql php-pecl-xdebug
+    sudo systemctl enable docker
+    sudo systemctl start docker
+    sudo systemctl enable httpd
+    sudo systemctl start httpd
+elif [ -n "$(command -v apt-get)" ]; then
+    sudo apt-get update >/dev/null
+    sudo apt-get install -y git zsh
+fi
+
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+sudo chmod +x /usr/local/bin/composer
+composer config -g repositories.packagist composer https://packagist.jp
+composer global require hirak/prestissimo
+
+sudo docker run -p 3306:3306 --name mysqld -e MYSQL_USER=homestead -e MYSQL_PASSWORD=secret -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=homestead -d mysql
+composer install
+cp .env.example .env
+php artisan key:generate
+php artisan config:clear
+
