b9d49d0390365808c053be13b4661ba58ecfb4d0
d9magai <kumagai@sencorp.co.jp>
2017-07-03 06:40:06 +0000

    if composer already exist, run self-update

--- run.sh
+++ run.sh
@@ -15,11 +15,15 @@ elif [ -n "$(command -v apt-get)" ]; then
     sudo apt-get install -y git zsh
 fi
 
-curl -sS https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/local/bin/composer
-sudo chmod +x /usr/local/bin/composer
-composer config -g repositories.packagist composer https://packagist.jp
-composer global require hirak/prestissimo
+if type "composer" > /dev/null 2>&1; then
+    composer self-update
+else
+    curl -sS https://getcomposer.org/installer | php
+    sudo mv composer.phar /usr/local/bin/composer
+    sudo chmod +x /usr/local/bin/composer
+    composer config -g repositories.packagist composer https://packagist.jp
+    composer global require hirak/prestissimo
+fi
 
 sudo docker run -p 3306:3306 --name mysqld -e MYSQL_USER=homestead -e MYSQL_PASSWORD=secret -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=homestead -d mysql
 composer install
